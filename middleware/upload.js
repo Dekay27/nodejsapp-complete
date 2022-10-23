c3aea4c6bf695987075bfa7f97ae8f64ed9cbf70
Dekay27 <dannykumah27@gmail.com>
2022-10-23 23:09:37 +0000

    --upload to multer not yet working

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,4 +1,31 @@
 const path = require('path')
 const multer = require('multer')
 
-var storage = multer.diskStorage()
+var storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, 'uploads/')
+    },
+    filename: function(req, file, cb) {
+        let ext = path.extname(file.originalname)
+        cb(null, Date.now() + ext)
+    }
+})
+
+var upload = multer({
+    storage: storage,
+    fileFilter: function(req, file, callback){
+        if(
+            file.mimetype == "image/jpg" ||
+            file.mimetype == "image/png"
+        ){
+            callback(null, true)
+        }else{
+            console.log("Only image formats of PNG and JPG allowed.")
+        }
+    },
+    limits: {
+        fileSize: 1024 * 1024 * 2
+    }
+})  
+
+module.exports = upload 
