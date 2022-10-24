2333669cb87e66a5ff1204ff64e4daeceeea2328
Dekay27 <dannykumah27@gmail.com>
2022-10-24 13:18:37 +0000

    -- updated terminal_scripts.js

--- terminal_scripts.js
+++ terminal_scripts.js
@@ -5,7 +5,8 @@
  * END PROCESS ON PORT 3000
  * kill -9 $(lsof -ti:3000)
  * 
- * 
+ * git config --global user.email "dannykumah27@gmail.com"
+ * git config --global user.name "Dekay27"
  */
 
 // 
